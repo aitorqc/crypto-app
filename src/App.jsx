dfeb9c1376a95e1c2bd1c2eff28444c35b498ec0
aquicor <aquinoacordero@gmail.com>
2022-11-11 14:51:39 +0100

    v.1.1 deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import CoinDetails from "./pages/CoinDetails";
 import Coins from "./pages/Coins";
@@ -9,12 +9,16 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/coins" element={<Coins/>}/>
-        <Route exact path="/exchanges" element={<Exchanges/>}/>
-        <Route exact path="/coins/:id" element={<CoinDetails/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/coins" element={<Coins />} />
+        <Route exact path="/exchanges" element={<Exchanges />} />
+        <Route exact path="/coins/:id" element={<CoinDetails />} />
+        <Route
+          path="*"
+          element={<Navigate to="/" replace />}
+        />
       </Routes>
     </>
   );
